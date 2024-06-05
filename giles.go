36ae688d2f781ef2c91a23d002107806440b8108
Thomas Price <pablo@cbrz.com>
2024-06-04 20:28:15 -0400

    Removed commented code

--- giles.go
+++ giles.go
@@ -23,7 +23,6 @@ func main() {
 
 func printProcessedFiles(progressCh <-chan int) {
 	for count := range progressCh {
-		//print("\r%d", count + " ")
 		fmt.Printf("\rProcessed %d files", count)
 	}
 }
@@ -35,43 +34,8 @@ func scanFiles(dirPath, extFilter string, progressCh chan<- int) {
 			return nil
 		}
 
-		//start := time.Now() // Get the current time before the hash calculation
-		//_, err = utils.Hash(path)
-		//duration := time.Since(start)
-		//if err != nil {
-		//	log.Printf("Error hashing %s: %v", path, err) // Log non-fatal errors
-		//	return nil
-		//}
-		//if duration > time.Second { // Check if duration is over 1 second
-		//fileSize := formatBytes(info.Size())
-		//processedFiles = fmt.Sprintf("%s (size: %s, hash time: %v)", path, fileSize, duration)
-		//processedFiles = fmt.Sprintf("%s (size: %s)", path, fileSize)
 		processedFiles++
 		progressCh <- processedFiles
-		//}
 		return nil
 	})
-}
-
-func formatBytes(bytes int64) string {
-	const (
-		_        = iota
-		KB int64 = 1 << (10 * iota)
-		MB
-		GB
-		TB
-	)
-
-	switch {
-	case bytes >= TB:
-		return fmt.Sprintf("%dTB", int64(bytes)/TB)
-	case bytes >= GB:
-		return fmt.Sprintf("%dGB", int64(bytes)/GB)
-	case bytes >= MB:
-		return fmt.Sprintf("%dMB", int64(bytes)/MB)
-	case bytes >= KB:
-		return fmt.Sprintf("%dKB", int64(bytes)/KB)
-	default:
-		return fmt.Sprintf("%dB", bytes)
-	}
 }
