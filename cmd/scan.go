0145ad8620a166d72ebec90488832adbbdd9f7a2
Thomas Price <pablo@cbrz.com>
2024-06-22 22:44:17 -0400

    Redo scan command

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,15 +1,16 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
 	"giles/database"
 	"giles/models"
+	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
 
-	"github.com/spf13/cobra"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var scanCmd = &cobra.Command{
@@ -21,11 +22,14 @@ The name, path and size are recorded.
 Usage: giles scan <directory>`,
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+
 		dirPath := "."
 		if len(args) > 0 {
 			dirPath = args[0]
 		}
-		scanDirectory(dirPath)
+
+		processDir(dirPath)
+		fmt.Println("Scan complete")
 	},
 }
 
@@ -33,43 +37,13 @@ func init() {
 	rootCmd.AddCommand(scanCmd)
 }
 
-const batchSize = 100
-
-func scanDirectory(dirPath string) {
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	fileDataCh := make(chan []models.FileData, 10)
-
-	go func() {
-		defer wg.Done()
-		insertToDatabase(fileDataCh)
-	}()
-
-	scanFiles(dirPath, fileDataCh)
-
-	close(fileDataCh)
-	wg.Wait()
-
-	fmt.Println("\nScanning complete.")
-}
-
-func insertToDatabase(fileDataCh <-chan []models.FileData) {
-	db, err := database.GetInstance()
+func processDir(path string) {
+	db, err := sql.Open("sqlite3", "./giles.db")
 	if err != nil {
-		log.Fatalf("Database error: %v", err)
+		panic(fmt.Errorf("error opening database: %v", err))
 	}
 
-	for files := range fileDataCh {
-		if err := db.InsertFiles(files); err != nil {
-			log.Printf("Error inserting files: \"%v\"", err)
-		}
-	}
-}
-
-func scanFiles(dirPath string, fileDataCh chan<- []models.FileData) {
-	fileBuffer := make([]models.FileData, 0, batchSize)
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Printf("Error walking path: \"%v\"", err)
 		}
@@ -80,19 +54,8 @@ func scanFiles(dirPath string, fileDataCh chan<- []models.FileData) {
 			Size: info.Size(),
 		}
 
-		fileBuffer = append(fileBuffer, fileData)
-		if len(fileBuffer) == batchSize {
-			fileDataCh <- fileBuffer
-			fileBuffer = fileBuffer[:0]
-		}
+		database.InsertFile(db, fileData)
+
 		return nil
 	})
-
-	if err != nil {
-		log.Printf("Error during file walk: \"%v\"", err)
-	}
-
-	if len(fileBuffer) > 0 {
-		fileDataCh <- fileBuffer
-	}
 }
