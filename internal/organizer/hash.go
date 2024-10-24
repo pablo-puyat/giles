8f8769c98f687f87467cad3b00513f07329190f2
Thomas Price <pablo@cbrz.com>
2024-10-23 21:12:12 -0400

    WIP

--- internal/organizer/hash.go
+++ internal/organizer/hash.go
@@ -1,7 +1,10 @@
 package organizer
 
 import (
+	"fmt"
 	"giles/models"
+	"io"
+	"os"
 )
 
 func NewOrganizer(sourceDir string) *Organizer {
@@ -21,6 +24,46 @@ func (ds *Organizer) OrganizeFiles(files []models.FileData, destination string) 
 		directory := file.Hash[:n]
 		files[i].Name = file.Hash
 		files[i].Path = destination + "/" + directory
+		newName := files[i].Path + files[i].Name
+		// verify that source files exists
+
+		// check if directory exists and create if it doesn't
+		if _, err := os.Stat("data/csv_data"); os.IsNotExist(err) {
+			err := os.MkdirAll("data/json_data", 0755)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Directory Created Successfully")
+		} else {
+			fmt.Println("Directory exists")
+		}
+		// copy file
+		//copyFile(files[i].Path + files[i].Name)
+		// verify that file exists in new location
+
+		// remove previous file
+
 	}
 	return files
+}
+
+func copyFile(source string, destination string) error {
+	srcFile, err := os.Open(source)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Copy done!")
+	return nil
 }
